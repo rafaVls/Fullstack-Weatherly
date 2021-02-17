eba67a18521df231ceec16d3c42e617e4def36fd
Rafael Aviles <rafa.vfierro@outlook.com>
2021-02-17 11:26:15 -0800

    Initial commit.

---
+++ server.ts
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const colors = require("colors/safe");
+
+dotenv.config({ path: "./config/config.env" });
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("frontend/build"));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+	);
+}
+
+app.listen(
+	PORT,
+	console.log(
+		colors.green.bold(
+			`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+		)
+	)
+);
