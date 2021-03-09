85fe55ffa91f31dbdd7b228682ccf88a144b0e52
Rafael Aviles <rafa.vfierro@outlook.com>
2021-03-08 18:14:59 -0800

    refactor: :wastebasket: Clean up CurrentData from unused code.

--- frontend/src/components/CurrentData.tsx
+++ frontend/src/components/CurrentData.tsx
@@ -1,36 +1,12 @@
-import React, { useContext } from "react";
-import { GlobalContext } from "../context/GlobalState";
+import React from "react";
 import ForecastItems from "./ForecastItems";
 import MainContent from "./MainContent";
-
-import { getTodayString } from "../utils/date";
-import { getStateShort, getDayIcon } from "../utils/helperMethods";
 
 import styles from "../styles/CurrentData.module.css";
 
 export default function CurrentData() {
-	// const { forecast, cityInfo } = useContext(GlobalContext);
-	// const currentWeather = forecast.current.weather[0];
-	// const todaysTemperature = forecast.daily[0].temp;
-
 	return (
 		<section className={styles.dataContainer}>
-			{/* <section className={styles.mainContent}>
-				<img
-					src={getDayIcon(currentWeather.icon)}
-					title={currentWeather.main}
-					alt={`Current weather condition: ${currentWeather.description}`}
-				/>
-				<hgroup className={styles.temperatures}>
-					<h1>{forecast.current.temp} °K</h1>
-					<h2 title="Min temperature">Min: {todaysTemperature.min} °K</h2>
-					<h2 title="Max temperature">Max: {todaysTemperature.max} °K</h2>
-				</hgroup>
-				<h3>
-					{getTodayString()} <br />
-					{cityInfo[0].long_name}, {getStateShort(cityInfo)}
-				</h3>
-			</section> */}
 			<MainContent />
 			<ForecastItems />
 		</section>
