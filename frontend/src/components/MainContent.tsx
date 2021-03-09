296c4cabba2e8d75d9661d0c393b4953527a5517
Rafael Aviles <rafa.vfierro@outlook.com>
2021-03-08 18:55:10 -0800

    refactor: :recycle: Move Temperatures (along with context and stylesheet) to its own component.

--- frontend/src/components/MainContent.tsx
+++ frontend/src/components/MainContent.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import WeatherIcon from "./WeatherIcon";
+import Temperatures from "./Temperatures";
 
 import { getTodayString } from "../utils/date";
 import { getStateShort } from "../utils/helperMethods";
@@ -11,16 +12,11 @@ export default function MainContent() {
 	const { forecast, cityInfo } = useContext(GlobalContext);
 
 	const currentWeather = forecast.current.weather[0];
-	const todaysTemperature = forecast.daily[0].temp;
 
 	return (
 		<section className={styles.mainContent}>
 			<WeatherIcon weather={currentWeather} />
-			<hgroup className={styles.temperatures}>
-				<h1>{forecast.current.temp} °K</h1>
-				<h2 title="Min temperature">Min: {todaysTemperature.min} °K</h2>
-				<h2 title="Max temperature">Max: {todaysTemperature.max} °K</h2>
-			</hgroup>
+			<Temperatures typeOfTemps="current" />
 			<h3>
 				{getTodayString()} <br />
 				{cityInfo[0].long_name}, {getStateShort(cityInfo)}
