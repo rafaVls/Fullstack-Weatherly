0b901a039eab93d76539203220fd49e2a3d9f9f6
Rafael Aviles <rafa.vfierro@outlook.com>
2021-03-10 15:09:56 -0800

    fix: :bug: Fix style for location search form.

--- frontend/src/components/MainContent.tsx
+++ frontend/src/components/MainContent.tsx
@@ -20,6 +20,21 @@ export default function MainContent() {
 				{getTodayString()} <br />
 				{cityInfo[0].long_name}, {getStateShort(cityInfo)}
 			</h3>
+
+			<form
+				onSubmit={e => {
+					e.preventDefault();
+				}}
+			>
+				<label htmlFor="citySelector">Search by city: </label>
+				<section className={styles.searchBar}>
+					<input type="search" name="citySelector" placeholder="City name" />
+					<img
+						src="https://www.flaticon.com/svg/vstatic/svg/751/751463.svg?token=exp=1615415272~hmac=45e52b37a69adc42b18414867920397f"
+						alt="A magnifying glass"
+					/>
+				</section>
+			</form>
 		</section>
 	);
 }
