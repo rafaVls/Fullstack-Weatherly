9f491b05c2621152b6862a7aefea4af5aa1b8baf
Rafael Aviles <rafa.vfierro@outlook.com>
2021-03-10 21:29:06 -0800

    feat: :sparkles: Add geocoding router with regex for city names.

--- routes/APICalls.ts
+++ routes/APICalls.ts
@@ -10,12 +10,12 @@ router.get("/onecall/:lat(-?[0-9]{0,2}[\.]?[0-9]{1,100})&:lon(-?[0-9]{1,3}[\.]?[
 		const {lat, lon, ...rest} = await response.json();
 
 		return res.status(200).json({
-			success:true,
+			success: true,
 			data:rest
 		});
 	} catch (err) {
 		return res.status(500).json({
-			success:false,
+			success: false,
 			error: err.message
 		});
 	}
@@ -29,16 +29,38 @@ router.get("/reverse/:lat(-?[0-9]{0,2}[\.]?[0-9]{1,100})&:lon(-?[0-9]{1,3}[\.]?[
 		const data = await response.json();
 
 		return res.status(200).json({
-			success:true,
+			success: true,
 			data: data.results[0].address_components
 		});
 	} catch (err) {
 		return res.status(500).json({
-			success:false,
+			success: false,
 			error: err.message
 		});
 	}
 });
+
+router.get("/geocoding/:cityName([A-Za-z]+(-[A-Za-z]+)?(-[A-Za-z]+)?(-[A-Za-z]+)?)", async (req, res) => {
+	try {
+		const response = await fetch(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${req.params.cityName}&key=${process.env.GEOCODING_API_KEY}`
+		);
+		const data = await response.json();
+
+		return res.status(200).json({
+			success: true,
+			data: {
+				latitude: data.results[0].geometry.location.lat, 
+				longitude: data.results[0].geometry.location.lng
+			}
+		});
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: err.message
+		});
+	}
+})
 
 export {};
 module.exports = router;
